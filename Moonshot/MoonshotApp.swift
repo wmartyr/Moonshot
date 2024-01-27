26f07786d6d6d4c75fe69a0ed92c9e140bf02169
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-28 09:17:37 +1030

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Woodrow Martyr on 28/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
